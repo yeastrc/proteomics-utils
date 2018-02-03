0136fc225d3ede757da9ebb06112ecb22913fa6e
Michael Riffle <mriffle@uw.edu>
2018-02-03 14:53:46 -0800

    Add unit tests to PeptideUtils

--- src/main/org/yeastrc/proteomics/peptide/peptide/PeptideUtils.java
+++ src/main/org/yeastrc/proteomics/peptide/peptide/PeptideUtils.java
@@ -7,7 +7,10 @@ import org.yeastrc.proteomics.peptide.aminoacid.InvalidAminoAcidException;
 
 public class PeptideUtils {
 
-	public static boolean isValidPeptideSequence( String sequence ) throws Exception {
+	public static boolean isValidPeptideSequence( String sequence ) {
+		
+		if( sequence == null || sequence.length() < 1 )
+			return false;
 		
 		Peptide peptide = new Peptide( sequence );
 		
@@ -23,12 +26,14 @@ public class PeptideUtils {
 	
 	/**
 	 * Check whether mod positions are legal--they cannot be less than 0 or greater than the length of the sequence
-	 * (0 is an n-terminal mod, max length is c-terminal mod)
+	 * (0 is an n-terminal mod, max length is c-terminal mod) If positions is empty or null, this will return true.
 	 * @param sequence
 	 * @param positions
 	 * @return
 	 */
 	public static boolean isValidPeptideModPositions( String sequence, Collection<Integer> positions ) {
+		
+		if( positions == null ) return true;
 		
 		int l = sequence.length();
 		
